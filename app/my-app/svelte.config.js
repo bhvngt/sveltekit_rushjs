0343d3308b676803816599a103d05d0ed25df1b1
bhvgt <bhavin.kamani@gembatech.com>
2022-04-21 14:43:05 +0530

    Initial Commit

---
+++ app/my-app/svelte.config.js
@@ -0,0 +1,35 @@
+import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+    // Consult https://github.com/sveltejs/svelte-preprocess
+    // for more information about preprocessors
+    preprocess: preprocess(),
+    kit: {
+        adapter: adapter(),
+        vite: () => ({
+            ssr: {
+                external: ["@lib/my-lib"]
+            },
+            resolve: {
+                alias: {
+                    "@lib/my-lib": "@lib/my-lib/package"
+                }
+            },
+            build: {
+                rollupOptions: {
+                    output: {
+                        manualChunks: {}
+                    }
+                }
+            },
+        }),
+        // Override http methods in the Todo forms
+        methodOverride: {
+            allowed: ['PATCH', 'DELETE']
+        }
+    }
+};
+
+export default config;
---
+++ lib/my-lib/src/lib/Demo.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import chunk from "lodash-es/chunk.js";
+
+    let example = chunk(['a', 'b', 'c', 'd'], 2);
+</script>
+
+<div>{example}</div>
